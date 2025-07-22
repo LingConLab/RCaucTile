ede59438967ab780d82e1884296cf0bea0776f51
George Moroz <agricolamz@gmail.com>
2025-07-22 10:08:37 +0300

    add an argument for the feature name

--- R/ec_tile_map.R
+++ R/ec_tile_map.R
@@ -1,6 +1,9 @@
 #' Draw a grid tile map for East Caucasian languages.
 #'
-#' @returns
+#' @param data Could be NULL, then it will print the language template. Otherwise should be a dataframe with language column annotated with some feature. Default value is NULL.
+#' @param feature_column Character vector of length 1 which specifies the column in dataframe that contains annotation for the feature to color the language template with.
+#'
+#' @returns a `ggplot2` object
 #' @export
 #'
 #' @examples
@@ -8,26 +11,41 @@
 #'
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 aes
+#' @importFrom ggplot2 annotate
 #' @importFrom ggplot2 geom_tile
+#' @importFrom ggplot2 geom_segment
 #' @importFrom ggplot2 scale_color_manual
+#' @importFrom ggplot2 scale_colour_discrete
 #' @importFrom ggplot2 theme_void
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 guides
 #' @importFrom dplyr left_join
+#' @importFrom dplyr if_else
 #' @importFrom dplyr mutate
 #' @importFrom dplyr rowwise
+#' @importFrom dplyr rename
 #' @importFrom forcats fct_inorder
+#' @importFrom grDevices col2rgb
 
-ec_tile_map <- function(data = NULL) {
+ec_tile_map <- function(data = NULL, feature_column = "feature") {
   # Check whether data are provided
   if(!is_null(data)){
-    # stop if there is no languages or feature columns in ec_languages df
-    stopifnot("Data should be a dataframe with columns language and feature" =
+    # Arguments check
+    stopifnot("Data should be a dataframe" =
                 "data.frame" %in% class(data),
-              "Data should be a dataframe with columns language and feature" =
+              "Data should contain column 'language'" =
                 "language" %in% colnames(data),
-              "Data should be a dataframe with columns language and feature" =
-                "feature" %in% colnames(data))
+              "Data should contain column 'feature'. If you have a different
+              name, please, use the argument 'feature_column' to provide it." =
+                feature_column %in% colnames(data),
+              "The argument 'feature_column' should be a character vector with
+              one value" =
+                length(feature_column) == 1)
+
     ec_languages |>
       dplyr::left_join(data, by = "language") |>
+      dplyr::rename(feature = {{feature}}) |>
       dplyr::rowwise() |>
       dplyr::mutate(red = grDevices::col2rgb(lang_col)[1],
                     green = grDevices::col2rgb(lang_col)[2],
@@ -58,17 +76,20 @@ ec_tile_map <- function(data = NULL) {
                                          y=y+0.5, yend=y+0.5),
                             linewidth=3) +
       ggplot2::annotate(geom = "text",
-                        x = for_plot |> filter(text_color == "#000000") |> pull(x),
-                        y = for_plot |> filter(text_color == "#000000") |> pull(y),
-                        label = for_plot |> filter(text_color == "#000000") |> pull(language), color = "#000000")+
+                        x = for_plot[for_plot$text_color == "#000000",]$x,
+                        y = for_plot[for_plot$text_color == "#000000",]$y,
+                        label = for_plot[for_plot$text_color == "#000000",]$language,
+                        color = "#000000")+
       ggplot2::annotate(geom = "text",
-                        x = for_plot |> filter(text_color == "#ffffff") |> pull(x),
-                        y = for_plot |> filter(text_color == "#ffffff") |> pull(y),
-                        label = for_plot |> filter(text_color == "#ffffff") |> pull(language), color = "#ffffff")+
+                        x = for_plot[for_plot$text_color == "#ffffff",]$x,
+                        y = for_plot[for_plot$text_color == "#ffffff",]$y,
+                        label = for_plot[for_plot$text_color == "#ffffff",]$language,
+                        color = "#ffffff")+
       ggplot2::annotate(geom = "text",
-                        x = for_plot |> filter(text_color == "grey70") |> pull(x),
-                        y = for_plot |> filter(text_color == "grey70") |> pull(y),
-                        label = for_plot |> filter(text_color == "grey70") |> pull(language), color = "grey70")+
+                        x = for_plot[for_plot$text_color == "grey70",]$x,
+                        y = for_plot[for_plot$text_color == "grey70",]$y,
+                        label = for_plot[for_plot$text_color == "grey70",]$language,
+                        color = "grey70")+
       ggplot2::theme_void()+
       ggplot2::scale_fill_manual(values = ec_languages$lang_col)+
       ggplot2::scale_colour_discrete(na.translate = FALSE)+
