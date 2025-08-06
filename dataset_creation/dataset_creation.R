754e1631947a481ff865f5530124c6095a342d4d
George Moroz <agricolamz@gmail.com>
2025-08-06 18:41:02 +0300

    add ordering to ec_languages; add section on value ordering

--- dataset_creation/dataset_creation.R
+++ dataset_creation/dataset_creation.R
@@ -1,2 +1,6 @@
 ec_languages <- read.csv("dataset_creation/ec_languages.csv", na.strings = "")
+ec_languages$morning_greetings <- factor(ec_languages$morning_greetings,
+                                         levels = c("Did you wake up?",
+                                                    "Good morning",
+                                                    "Both"))
 save(ec_languages, file="data/ec_languages.rda", compress='xz')
